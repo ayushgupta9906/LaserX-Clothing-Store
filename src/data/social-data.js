9bebee504d172b5be83dc572068f9b044792a700
saezhcetamu <sae@zhcet.ac.in>
2024-07-26 16:57:51 +0530

    first commit

---
+++ src/data/social-data.js
@@ -0,0 +1,24 @@
+
+const social_data = [
+  {
+    id:1,
+    link:'https://www.instagram.com/LaSerX_1729/',
+    icon:'fa-brands fa-instagram',
+    title:'Facebook'
+  },
+  {
+    id:2,
+    link:'https://x.com/LaSerX_1729',
+    icon:'fa-brands fa-twitter',
+    title:'Twitter'
+  },
+  {
+    id:3,
+    link:'https://www.linkedin.com/in/ayush-gupta-3a93621b6/',
+    icon:'fa-brands fa-linkedin-in',
+    title:'Linkedin'
+  },
+  
+]
+
+export default social_data;
---
+++ src/data/testimonial-data.js
@@ -0,0 +1,60 @@
+import user_1 from '@assets/img/users/user-1.jpg';
+import user_2 from '@assets/img/users/user-2.jpg';
+import user_3 from '@assets/img/users/user-3.jpg';
+import user_4 from '@assets/img/users/user-4.jpg';
+
+// fashion_testi_data
+export const fashion_testi_data = [
+  {
+    id:1,
+    review:4,
+    desc:'',
+    user:user_2,
+    name:'',
+    designation:''
+  },
+  {
+    id:2,
+    review:5,
+    desc:'',
+    user:user_3,
+    name:'',
+    designation:''
+  },
+  {
+    id:3,
+    review:3,
+    desc:'',
+    user:user_4,
+    name:'',
+    designation:''
+  },
+]
+
+// beauty testimonial data 
+export const beauty_testi_data = [
+  {
+    id:1,
+    review:4,
+    desc:'',
+    user:user_1,
+    name:'',
+    designation:''
+  },
+  {
+    id:2,
+    review:3.5,
+    desc:'',
+    user:user_2,
+    name:'',
+    designation:''
+  },
+  {
+    id:3,
+    review:5,
+    desc:'',
+    user:user_3,
+    name:'',
+    designation:''
+  },
+]
