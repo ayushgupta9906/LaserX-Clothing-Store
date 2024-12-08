e475711ac259c182746982244780409755e0be82
ayushgupta9906 <ayush150152@gmail.com>
2024-12-09 00:59:08 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['i.ibb.co','lh3.googleusercontent.com','res.cloudinary.com','plus.unsplash.com','cdn.britannica.com','js.stripe.com'],
+  },
+}
+
+module.exports = nextConfig
---
+++ src/components/contact/contact-area.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import Image from "next/image";
+// internal
+import ContactForm from "../forms/contact-form";
+import contact_icon_1 from "@assets/img/contact/contact-icon-1.png";
+import contact_icon_2 from "@assets/img/contact/contact-icon-2.png";
+import contact_icon_3 from "@assets/img/contact/contact-icon-3.png";
+
+const ContactArea = () => {
+  return (
+    <>
+      <section className="tp-contact-area pb-100">
+        <div className="container">
+          <div className="tp-contact-inner">
+            <div className="row">
+              <div className="col-xl-9 col-lg-8">
+                <div className="tp-contact-wrapper">
+                  <h3 className="tp-contact-title">Sent A Message</h3>
+
+                  <div className="tp-contact-form">
+                    {/* form start */}
+                    <ContactForm />
+                    {/* form end */}
+                    <p className="ajax-response"></p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xl-3 col-lg-4">
+                <div className="tp-contact-info-wrapper">
+                  <div className="tp-contact-info-item">
+                    <div className="tp-contact-info-icon">
+                      <span>
+                        <Image src={contact_icon_1} alt="contact-icon" />
+                      </span>
+                    </div>
+                    <div className="tp-contact-info-content">
+                      <p data-info="mail">
+                        <a href="mailto:contact@shofy.com">contact@shofy.com</a>
+                      </p>
+                      <p data-info="phone">
+                        <a href="tel:91 7906 150 152">+91 7906 150 152</a>
+                      </p>
+                    </div>
+                  </div>
+                  <div className="tp-contact-info-item">
+                    <div className="tp-contact-info-icon">
+                      <span>
+                        <Image src={contact_icon_2} alt="contact-icon" />
+                      </span>
+                    </div>
+                    <div className="tp-contact-info-content">
+                      <p>
+                        <a
+                          href="https://maps.app.goo.gl/e4AiMVCr4fLzDq817"
+                          target="_blank"
+                        >
+                          105/21 Green Park Colony
+                         <br />  Bareilly, Uttar Pradesh, India 243006
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                  <div className="tp-contact-info-item">
+                    <div className="tp-contact-info-icon">
+                      <span>
+                        <Image src={contact_icon_3} alt="contact-icon" />
+                      </span>
+                    </div>
+                    <div className="tp-contact-info-content">
+                      <div className="tp-contact-social-wrapper mt-5">
+                        <h4 className="tp-contact-social-title">
+                          Find on social media
+                        </h4>
+
+                        <div className="tp-contact-social-icon">
+                          <a href="#">
+                            <i className="fa-brands fa-facebook-f"></i>
+                          </a>
+                          <a href="#">
+                            <i className="fa-brands fa-twitter"></i>
+                          </a>
+                          <a href="#">
+                            <i className="fa-brands fa-linkedin-in"></i>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ContactArea;
