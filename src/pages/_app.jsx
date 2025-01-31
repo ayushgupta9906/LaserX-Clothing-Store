9be5940bbf8e97f7eea0cfdb1bb278791402bd97
ayushgupta9906 <ayush150152@gmail.com>
2025-01-31 10:09:06 +0530

    Projects

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -13,10 +13,10 @@ if (typeof window !== "undefined") {
   ReactModal.setAppElement("body");
 }
 
-// stripe
+// stripePromise
 const NEXT_PUBLIC_STRIPE_KEY = 'pk_test_51NYXCFGndYsQkAEFifIbJH64sZFMDpF7DcLYvUUN2az3VdK1M7qVPo7Z2j9rhunf3Pd0C3aFLENIxFriJWwx1P6a00lQFqaoc6';
 const stripePromise = loadStripe(NEXT_PUBLIC_STRIPE_KEY);
-const NEXT_PUBLIC_GOOGLE_CLIENT_ID = '263249757667-ipffv97o0f6o85isq24rafulo5vs9uta.apps.googleusercontent.com'
+const NEXT_PUBLIC_GOOGLE_CLIENT_ID = '903081644488-n60j77h2s46n2anpau0p5krrjrpi8aae.apps.googleusercontent.com'
 export default function App({ Component, pageProps }) {
   return (
     <GoogleOAuthProvider clientId={NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
